6f7b37cc1aedb0cc42b152d6e84e964e8216281e
Shagun-tom <shaguntom123@gmail.com>
2025-08-31 21:14:31 +0530

    javascript file

---
+++ dropdown.js
@@ -0,0 +1,6 @@
+const dropdw = document.querySelector('.dropdown-content');
+const btn = document.querySelector('.dropdwbtn');
+
+btn.addEventListener('click',function(){
+       dropdw.classList.toggle('show');
+});
